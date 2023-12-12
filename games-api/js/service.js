f63a31580ee5addc9a4e70f705a46abbad755256
Davi Duarte <davi.d.santos8@aluno.senai.br>
2023-12-12 18:36:06 -0300

    jogos

---
+++ games-api/js/service.js
@@ -0,0 +1,24 @@
+import { handleErros } from "./exception.js";
+var URL = 'http://localhost:3000/jogos'
+
+export const getAllGames = async () => {
+    try {
+        // Fazendo uma solicitação GET para obter produtos da AP
+        const response =
+            await fetch(URL);
+        //lidando com oerros na resposta
+        handleErrors(response);
+
+        //converter os dados para json
+        const data = await response.json();
+
+    } catch (error) {
+        console.log('Error >>>', error);
+    }
+};
+
+
+  
+        
+   
+
